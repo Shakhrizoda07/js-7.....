34691614c73a6df2d8224fdb071386740aecc6b9
Shakhrizoda07 <shakhrizoda752@gmail.com>
2025-01-29 08:19:06 -0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,58 @@
+// let i = 0;
+// while(i < 3){
+//     console.log(i)
+//     i++
+// }   // iteratsiyadan oldin tekshiradi
+
+// let i = 0;
+// do{
+//     console.log(i);
+//     i++;
+// }while(0);  //iteratsiyadan keyn tekshiradi
+
+
+// for (i = 0; i < 10; i++){
+//     if(i == 7)break;  //kodi toxtadi
+//     console.log(i)
+// }   
+
+// let sum = 0;
+// while(true){
+//     let value = +prompt('Введите число');
+//     if (!value) break;
+//     sum += value;
+// }
+// console.log('Сумма:' + sum);
+
+// for(i = 0; i < 100; i++){
+//     if(i % 2 ==0) continue;
+//     console.log(i)  //toq sonla или чётные числа
+// }
+
+// for (i = 0; i <= 100; i++){
+//     console.log(i) 
+// }
+
+// for (i = 0; i <= 100; i += 2) {
+//     console.log(i);
+// }
+
+
+let number = ['2', '4', '9', '10', '45', '67', '8', '90'];
+console.log(number);
+
+let numberTwo = ['2', '4', '10', '8', '90'];
+console.log(numberTwo)
+
+
+// let numbers = [2, 4, 9, 10, 45, 67, 8, 90]; 
+// console.log(numbers); 
+
+// let userNumbers = numbers.filter(num => num % 2 === 0); 
+// console.log(userNumbers); 
+
+
+// for (i = 0; i <= 100; i += 2) {
+//     console.log(i);
+// }
+
